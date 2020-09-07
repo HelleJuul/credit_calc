523d6eb19d17f3dd031024bd604e243ab41265d8
Helle Juul Hansen <hellejuulpedersen@gmail.com>
2020-09-07 21:21:12 +0200

    Diff loans and argparse
    Now the program accepts arguments passed to it in the shell. Plus it can calculate differentiated payment.

--- credit_calc.py
+++ credit_calc.py
@@ -1,24 +1,29 @@
 import math
 
-# A = annuity payment
-# P = credit principal
-# i = nominal (monthly) interest rate. Is a floating value (not percent)
-# n = number of payments (i.e. number of months)
+import argparse
 
-print("What do you want to calculate?")
-print('type "n" for number of monthly payments,')
-print('type "a" for annuity monthly payment amount,')
-print('type "p" for credit principal:')
+parser = argparse.ArgumentParser()
 
-answer = input()
+parser.add_argument("--type", help="select between differentiate payment: 'diff' and annuity payment: 'annuity'")
 
-if answer == "n":       # We want to calculate n
-    print("Enter the credit principal:")
-    P = float(input())
-    print("Enter the monthly payment:")
-    A = float(input())
-    print("Enter the credit interest:")
-    interest = float(input())
+parser.add_argument("--payment", type=int, help="the monthly payment")
+
+parser.add_argument("--principal", type=int, help="the credit principal")
+
+parser.add_argument("--periods", type=int, help="the number of payments to repay the credit")
+
+parser.add_argument("--interest", type=float, help="the yearly interest as percent; no percentage sign")
+
+args = parser.parse_args()
+
+
+
+def calculate_number_of_payments(principal, payment, interest):
+    """Calculate the number of payments needed to repay a loan given a credit
+    principal, a monthly payment and an annual interest"""
+
+    P = principal
+    A = payment
 
     # calculate the monthly interest rate as a decimal number
     i = (interest / 100) / 12
@@ -44,14 +49,35 @@ if answer == "n":       # We want to calculate n
         months = n % 12
         print(f"It will take {years} years and {months} months to repay this credit!")
 
+    calculate_overpayment(P, n, A)
 
-elif answer == "a":     # We want to calculate A
-    print("Enter the credit principal:")
-    P = float(input())
-    print("Enter the number of periods:")
-    n = int(input())
-    print("Enter the credit interest:")
-    interest = float(input())
+
+
+def calculate_principal(payment, periods, interest):
+    """Calculate the principal given the monthly payment, the total number of
+    payments and the annual interest."""
+    
+    A = payment
+    n = periods
+   
+    # Calculate the monthly interest as a decimal number
+    i = (interest / 100) / 12
+
+    # Calculate the principal credit
+    P = A / ((i * math.pow(1 + i, n)) / (math.pow(1 + i, n) - 1))
+    
+    print(f"Your credit principal = {round(P)}!")
+    
+    calculate_overpayment(round(P), n, A)
+
+
+
+def calculate_payment(principal, periods, interest):
+    """Calculate the monthly payment given the credit principal, the total 
+    number of payments and the annual interest."""
+
+    P = principal
+    n = periods
 
     # Calculate the monthly interest as a decimal number
     i = (interest / 100) / 12
@@ -61,18 +87,86 @@ elif answer == "a":     # We want to calculate A
 
     print(f"Your monthly payment = {math.ceil(A)}!")
 
-elif answer == "p":     # We want to calculate P
-    print("Enter the annuity payment:")
-    A = float(input())
-    print("Enter the number of periods:")
-    n = int(input())
-    print("Enter the credit interest:")
-    interest = float(input())
+    calculate_overpayment(P, n, math.ceil(A))
 
-    # Calculate the monthly interest as a decimal number
+
+def calculate_overpayment(principal, periods, payment):
+    """Calculate how much much it cost you to take the loan"""
+
+    overpayment = periods * payment - principal
+
+    print(f"Overpayment = {overpayment}")
+
+
+def calculate_differentiated_payments(principal, periods, interest):
+    """Calculate and print the differentiated payments"""
+
+    P = principal
+    n = periods
+
+    # calculate the monthly interest as a decimal number
     i = (interest / 100) / 12
 
-    # Calculate the principal credit
-    P = A / ((i * math.pow(1 + i, n)) / (math.pow(1 + i, n) - 1))
+    total_payments = 0
 
-    print(f"Your credit principal = {round(P)}!")
+    # The monthly payment is called D
+    for m in range(1, n + 1):
+        D = P / n + i * (P - P * (m - 1) / n)
+
+        total_payments += math.ceil(D)
+
+        print(f"Month {m}: payment is {math.ceil(D)}")
+    
+    print(f"\nOverpayment = {round(total_payments - P)}")
+
+
+
+# Check how many arguments have been given when starting the program
+# And checking that no arguments are negative.
+count = 0
+if args.type:
+    count += 1
+if args.payment:
+    if args.payment <= 0:
+        print("Incorrect parameters")
+    count += 1
+if args.principal:
+    if args.principal <= 0:
+        print("Incorrect parameters")
+    count += 1
+if args.periods:
+    if args.periods <= 0:
+        print("Incorrect parameters")
+    count += 1
+if args.interest:
+    if args.interest <= 0:
+        print("Incorrect parameters")
+    count += 1
+
+
+if count < 4:                     
+    print("Incorrect parameters")
+
+else:
+    if args.type == "diff":
+        if args.payment:
+            print("Incorrect parameters")
+        else:
+            calculate_differentiated_payments(args.principal, args.periods, args.interest)
+
+    elif args.type == "annuity":
+        if not args.interest:
+            print("Incorrect parameters")
+        elif not args.periods:
+            calculate_number_of_payments(args.principal, args.payment, args.interest)
+
+        elif not args.principal:
+            calculate_principal(args.payment, args.periods, args.interest)
+
+        elif not args.payment:
+            calculate_payment(args.principal, args.periods, args.interest)
+
+    else:
+        print("Incorrect parameters")
+
+
